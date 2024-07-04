41a9b33d83f46d94521f0eb27f76662796119efc
Himanshu singh <himanshu.singh@simplyentertaining.us>
2024-07-04 09:16:27 +0530

    Initial Commit

---
+++ LocationTracker2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationTracker2
+//
+//  Created by HKBeast on 04/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LocationTracker2Tests/LocationTracker2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  LocationTracker2Tests.swift
+//  LocationTracker2Tests
+//
+//  Created by HKBeast on 04/07/24.
+//
+
+import Testing
+@testable import LocationTracker2
+
+struct LocationTracker2Tests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
